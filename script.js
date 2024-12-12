8139d66c8a1a232a64649c8fa455443a2f6f2a31
Hrushikesh <sonuhrushikeshshk.9999@gmail.com>
2024-12-12 17:29:44 +0530

    new

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ next.addEventListener('click', () => {
   currentIndex = (currentIndex + 1) % totalSlides;
   updateSliderPosition();
 });
+
+
+// typing 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const text = `Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
+        therefore always free from repetition, injected humour, or
+        non-characteristic words, etc.
+        therefore always free from repetition, injected humour, or
+        non-characteristic words, etc.`;
+
+  let index = 0;
+  const typingElement = document.getElementById('typing-effect');
+  const typingSpeed = 100; // Speed of typing in ms
+
+  function typeWriter() {
+    if (index < text.length) {
+      typingElement.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, typingSpeed);
+    }
+  }
+
+  typeWriter();
+});
+
+
+
+// menu toggle  
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
+});
